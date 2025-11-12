ea723aafe7be3c295ee6578a1f18bac46d715bcc
fasnfilho <fabinhosnf@gmail.com>
2025-11-12 12:53:27 -0300

    Colocando as bibliotecas que serão usadas

---
+++ cliente_string.py
@@ -0,0 +1,9 @@
+#Cliente para o protocolo Strings
+#Autor: Fábio Agostinho da Silva Nascimento Filho (Matrícula: 538521)
+#Servidor: 3.88.99.255:8080
+
+import socket
+import tkinter as tk
+from tkinter import ttk, messagebox, scrolledtext
+from datetime import datetime
+import hashlib
